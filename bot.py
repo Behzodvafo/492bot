2bdb8da4fc39e8f941bd9b2ca7aa7b2606c683f7
Behzodvafo <102291658+Behzodvafo@users.noreply.github.com>
2024-11-17 14:36:27 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,13 @@
-from flask import Flask
-from threading import Thread
+import os
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from handlers import start, handle_message, broadcast_message
 
-# Telegram Bot Token из переменных окружения
-import os
+# Загрузка переменной окружения для токена
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 if not TOKEN:
     raise ValueError("Bot token not found. Please set TELEGRAM_BOT_TOKEN as an environment variable.")
 
-# Flask-приложение для проверки доступности
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Bot is running", 200
-
-def run_flask():
-    # Flask будет слушать запросы на порту 5000
-    app.run(host="0.0.0.0", port=5000)
-
 def main():
-    # Telegram Bot Setup
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
@@ -30,14 +16,8 @@ def main():
     dp.add_handler(CommandHandler("broadcast", broadcast_message))
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
 
-    # Запуск Telegram-бота
+    # Запуск бота
     updater.start_polling()
-
-    # Запуск Flask-сервера в отдельном потоке
-    flask_thread = Thread(target=run_flask)
-    flask_thread.start()
-
-    # Ожидание завершения работы бота
     updater.idle()
 
 if __name__ == "__main__":
