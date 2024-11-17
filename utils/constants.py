f9882ca3f57e5427120a34f2d20aa93b4e3b3df0
Behzodvafo <102291658+Behzodvafo@users.noreply.github.com>
2024-11-17 10:55:58 -0500

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,1 +1,21 @@
+# Маппинг типов автомобилей
+CAR_TYPE_MAPPING = {
+    "1": "Sedan",
+    "2": "Luxury Sedan",
+    "3": "Minivan",
+    "4": "Van",
+    "8": "SUV",
+    "17": "Wheelchair"
+}
 
+# Маппинг компаний
+COMPANY_MAPPING = {
+    "1": "NYC2WAY",
+    "2": "ARISTACAR",
+    "8": "ALLCOUNTY",
+    "13": "ECONOMYCAR",
+    "9": "Excelsior",
+    "3": "TWR",
+    "5": "ALLSTATE",
+    "17": "FNST"
+}
